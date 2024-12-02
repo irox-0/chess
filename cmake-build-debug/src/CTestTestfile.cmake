bcfbfba8a8ce2b7ee30a95024aa269bb12628c47
irox_0 <kirik.usov@gmail.com>
2024-12-02 13:55:27 +0300

    Cleaning

--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp/chess1/src
-# Build directory: D:/cpp/chess1/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cpp/chess1/tests
-# Build directory: D:/cpp/chess1/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/cpp/chess1/cmake-build-debug/tests/chess_tests[1]_include.cmake")
